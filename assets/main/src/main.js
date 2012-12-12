ec3a5af9f1957dffc6dcc6c750604716342d9215
diguage <leejun119@gmail.com>
2012-12-12 16:49:26 +0800

    鎸夌収SeaJS瑙勮寖锛屼功鍐欒嚜宸辩殑妯″潡

--- assets/main/src/main.js
+++ assets/main/src/main.js
@@ -1,7 +1,22 @@
 define(function(require) {
+    var $ = require('jquery');
+	var GuessNumber = require("./GuessNumber");
 
-    var $ = require('jquery')
+	var guess = new GuessNumber(201212);
 
-    
-   
+	guess.setNumberScopeWithNumberLength(2);
+	guess.start();
+	document.getElementById("showNumber").value = guess._nowNumber;
+
+	alert("Scope："+guess._numberScope+"，Max："+guess._maxNumber+"，Min："+guess._minNumber+"，Now："+guess._nowNumber+"，Count："+guess._count);
+	$("#maxButton").click(function(){
+		guess.toSmaller();
+		document.getElementById("showNumber").value = guess._nowNumber;
+	});
+
+	$("#minButton").click(function(){
+		guess.toLarger();
+		document.getElementById("showNumber").value = guess._nowNumber;
+	});
+
 })
