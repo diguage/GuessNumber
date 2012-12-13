8a934b64d88cdf983654f0072013f8df61b265da
diguage <leejun119@gmail.com>
2012-12-13 21:28:08 +0800

    做个微调

--- assets/main/src/main.js
+++ assets/main/src/main.js
@@ -4,19 +4,18 @@ define(function(require) {
 
 	var guess = new GuessNumber(201212);
 
-	guess.setNumberScopeWithNumberLength(2);
-	guess.start();
-	document.getElementById("showNumber").value = guess._nowNumber;
+	//guess.setNumberScopeWithNumberLength(2);
+	document.getElementById("showNumber").value = guess.getGuess();
 
 	alert("Scope："+guess._numberScope+"，Max："+guess._maxNumber+"，Min："+guess._minNumber+"，Now："+guess._nowNumber+"，Count："+guess._count);
 	$("#maxButton").click(function(){
 		guess.toSmaller();
-		document.getElementById("showNumber").value = guess._nowNumber;
+		document.getElementById("showNumber").value = guess.getGuess();
 	});
 
 	$("#minButton").click(function(){
 		guess.toLarger();
-		document.getElementById("showNumber").value = guess._nowNumber;
+		document.getElementById("showNumber").value = guess.getGuess();
 	});
 
 })
