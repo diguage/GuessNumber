a902178b99c0885c5d42c06203e1f47495f59799
diguage <leejun119@gmail.com>
2012-12-13 23:56:22 +0800

    增加日期猜测的适配。

--- assets/main/src/main.js
+++ assets/main/src/main.js
@@ -5,17 +5,30 @@ define(function(require) {
 	var guess = new GuessNumber(201212);
 
 	//guess.setNumberScopeWithNumberLength(2);
-	document.getElementById("showNumber").value = guess.getGuess();
+	//document.getElementById("showNumber").value = guess.getGuess();
 
-	alert("Scope："+guess._numberScope+"，Max："+guess._maxNumber+"，Min："+guess._minNumber+"，Now："+guess._nowNumber+"，Count："+guess._count);
+	var min = Math.round(new Date(0).getTime() / (1000*3600*24));
+	var max = Math.round(new Date().getTime() / (1000*3600*24));
+	var type = 2; //1 代表数字；2 代表日期
+	function resultToString(num, type) {
+		if(type === 1) {
+			return num;
+		} else if (type === 2) {
+			var tmp = new Date(num * 1000 * 3600 * 24);
+			return tmp.getFullYear()+"年"+(tmp.getMonth()+1)+"月"+tmp.getDate()+"日";
+		}
+	}
+	guess.start(min, max);
+	document.getElementById("showNumber").value = resultToString(guess.getGuess(), 2);
+	//alert("Scope："+guess._numberScope+"，Max："+guess._maxNumber+"，Min："+guess._minNumber+"，Now："+guess._nowNumber+"，Count："+guess._count);
 	$("#maxButton").click(function(){
 		guess.toSmaller();
-		document.getElementById("showNumber").value = guess.getGuess();
+		document.getElementById("showNumber").value = resultToString(guess.getGuess(), 2);
 	});
 
 	$("#minButton").click(function(){
 		guess.toLarger();
-		document.getElementById("showNumber").value = guess.getGuess();
+		document.getElementById("showNumber").value = resultToString(guess.getGuess(), 2);
 	});
 
 })
