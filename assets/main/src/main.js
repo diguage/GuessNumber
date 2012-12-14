103d703715f73563e555e20de2b2f05e9242b4f7
diguage <leejun119@gmail.com>
2012-12-14 13:20:23 +0800

    閲嶆瀯浠ｇ爜锛屼娇鍏舵洿瀹规槗鎵╁睍

--- assets/main/src/main.js
+++ assets/main/src/main.js
@@ -1,34 +1,59 @@
 define(function(require) {
     var $ = require('jquery');
 	var GuessNumber = require("./GuessNumber");
+	
+	//格式化显示结果
+	function formatResult(num, type) {
+		if (type == 1) {
+			var tmp = new Date(num * 1000 * 3600 * 24);
+			return tmp.getFullYear()+"年"+(tmp.getMonth()+1)+"月"+tmp.getDate()+"日";
+		} else {
+			var numStr = num + "";
+			var strLeng = numStr.length;
+			var result = "";
+			for(var i = strLeng; i >= 0; i -= 4) {
+				var startIndex = i-4 > 0 ? i-4 : 0;
+				result = numStr.substring(startIndex, i) + result;
+				if(i > 4) {
+					result = "-" + result;
+				}
+			}
+			return result;
+		}
+	}
 
 	var guess = new GuessNumber(201212);
 
-	//guess.setNumberScopeWithNumberLength(2);
-	//document.getElementById("showNumber").value = guess.getGuess();
+	// 定义个猜数范围数组
+	var scopeArr = [{min: 0, max: 100, tip: ""}, // 小试牛刀
+					{min: Math.round(new Date(0).getTime() / (1000*3600*24)), 
+						max: Math.round(new Date().getTime() / (1000*3600*24)), 
+						tip: ""}, // 猜猜生日
+					{min:Math.pow(10, 10), max: 2*Math.pow(10, 10), 
+						tip: ""} // 猜猜手机号
+				   ];
 
-	var min = Math.round(new Date(0).getTime() / (1000*3600*24));
-	var max = Math.round(new Date().getTime() / (1000*3600*24));
-	var type = 2; //1 代表数字；2 代表日期
-	function resultToString(num, type) {
-		if(type === 1) {
-			return num;
-		} else if (type === 2) {
-			var tmp = new Date(num * 1000 * 3600 * 24);
-			return tmp.getFullYear()+"年"+(tmp.getMonth()+1)+"月"+tmp.getDate()+"日";
-		}
-	}
-	guess.start(min, max);
-	document.getElementById("showNumber").value = resultToString(guess.getGuess(), 2);
+	var type = 0; //1 代表日期；其余代表数字
+
+	guess.start(scopeArr[type].min, scopeArr[type].max);
+	document.getElementById("showNumber").value = formatResult(guess.getGuess(), type);
+
+	$("#guessType").change(function(){
+		type = $(this).val();
+		guess.start(scopeArr[type].min, scopeArr[type].max);
+		document.getElementById("showNumber").value = formatResult(guess.getGuess(), type);
+	});
+	
+	document.getElementById("showNumber").value = formatResult(guess.getGuess(), type);
 	//alert("Scope："+guess._numberScope+"，Max："+guess._maxNumber+"，Min："+guess._minNumber+"，Now："+guess._nowNumber+"，Count："+guess._count);
 	$("#maxButton").click(function(){
 		guess.toSmaller();
-		document.getElementById("showNumber").value = resultToString(guess.getGuess(), 2);
+		document.getElementById("showNumber").value = formatResult(guess.getGuess(), type);
 	});
 
 	$("#minButton").click(function(){
 		guess.toLarger();
-		document.getElementById("showNumber").value = resultToString(guess.getGuess(), 2);
+		document.getElementById("showNumber").value = formatResult(guess.getGuess(), type);
 	});
 
 })
